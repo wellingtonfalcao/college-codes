45f54823a8f16f1462f3b9d289e4e8dd3f96f33d
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-07-31 21:49:50 -0300

    Update: added EventHander exercises

---
+++ c-sharp/delegate/Evento1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Evento1.Models;
+
+namespace Evento1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(1, "João", 1000.0);
+            Console.WriteLine(conta);
+            conta.AlterarSaldo += TratarAlteracaoSaldo; // Assinando o evento
+            conta.AlterarSaldo += TratarAlteracaoSaldoSMS; // E possivel fazer multicast
+            conta.Creditar(500.0); // Credita 500
+            Console.WriteLine(conta);
+            conta.Debitar(200.0); // Debita 200
+            Console.WriteLine(conta);
+        }
+
+        // Tratador de evento
+        public static void TratarAlteracaoSaldoSMS(object sender, EventArgs e)
+        {
+            Console.WriteLine($"SMS: Saldo alterado");
+        }
+        public static void TratarAlteracaoSaldo(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Saldo alterado");
+        }
+    }
+}
---
+++ c-sharp/delegate/Evento2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Evento2.Models;
+
+namespace Evento2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(1, "João", 1000.0);
+            Console.WriteLine(conta);
+            conta.AlterarSaldo += TratarAlteracaoSaldo; // Assinando o evento
+            conta.AlterarSaldo += TratarAlteracaoSaldoSMS; // Assinando o evento
+            conta.Creditar(500.0); // Credita 500
+            Console.WriteLine(conta);
+            conta.Debitar(2000.0); // Debita 200
+            Console.WriteLine(conta);
+        }
+
+        // Tratador de evento
+        public static void TratarAlteracaoSaldo(object sender, AlteracaoSaldoArgs e)
+        {
+            Console.WriteLine(e.Mensagem);
+        }
+
+        public static void TratarAlteracaoSaldoSMS(object sender, AlteracaoSaldoArgs e)
+        {
+            Console.WriteLine("SMS: " + e.Mensagem);
+        }
+    }
+}
