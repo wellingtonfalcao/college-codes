28da67f4eb0ff3a8eeeac28dba81e2fb8acc6d99
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-08-04 22:50:38 -0300

    Update: added MainLogin and Usuario classes to generate csv file to test reand and write

--- java/tp3-projeto-bloco-sistema-delivery/src/main/java/br/edu/infnet/apiwellington/controller/UsuarioController.java
+++ java/tp3-projeto-bloco-sistema-delivery/src/main/java/br/edu/infnet/apiwellington/controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package br.edu.infnet.apiwellington.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+
+    @GetMapping("/login")
+    public void GerarLerCSV() {
+        System.out.println("Teste");
+    }
 }
---
+++ java/tp3-projeto-bloco-sistema-delivery/src/main/java/br/edu/infnet/apiwellington/service/UsuarioService.java
@@ -0,0 +1,4 @@
+package br.edu.infnet.apiwellington.service;
+
+public class UsuarioService {
+}
