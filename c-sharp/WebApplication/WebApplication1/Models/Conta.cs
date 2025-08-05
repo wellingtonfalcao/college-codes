224a1a5584387dfa76389ee52ebffe47b894137a
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-08-05 12:18:40 -0300

    Update: maked 3 exercises to learn how to use View and Controller with Razor Pages

---
+++ c-sharp/WebApplication/WebApplication1/Models/Conta.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double saldo { get; set; }
+
+        public Conta(int id, string nome, double saldo)
+        {
+            Id = id;
+            Nome = nome;
+            this.saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Nome: {Nome}, Saldo: {saldo}";
+        }
+    }
+}
--- c-sharp/WebApplication/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ c-sharp/WebApplication/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5d34cecdbc0d0edb030ef66cdf19c9e766707b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
