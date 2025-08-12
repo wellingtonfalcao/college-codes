6216adc5d729e40ddcdf88c566f392878e60092e
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-08-12 20:12:09 -0300

    Update: exercise 3 to 6 of Java Spring Boot done.

--- java/tp1-desenv-java-wellington-falcao/src/test/java/tp1/wellingtonfalcao/teste/TesteApplicationTests.java
+++ java/tp1-desenv-java-wellington-falcao/src/test/java/tp1/wellingtonfalcao/teste/TesteApplicationTests.java
@@ -12,29 +12,4 @@ class TesteApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	void testAddition() {
-		ScientificCalculator calc = new ScientificCalculator();
-		double resultado = calc.add(5,5);
-		assertEquals(10, resultado);
-	}
-
-	@Test
-	void deveSubtrair_QuandoNumeros_EntaoRetornarSubtracaoCorreta() {
-		//Dado - Setup
-		ScientificCalculator calc = new ScientificCalculator();
-		double numero1 = 10;
-		double numero2 = 5;
-		double resultadoEsperado = 5;
-
-		//Quando - Execution
-		double resulSubt = calc.subtract(10,5);
-
-		//Então - Assertion
-		assertEquals(resultadoEsperado, resulSubt);
-	}
-
-
-
 }
