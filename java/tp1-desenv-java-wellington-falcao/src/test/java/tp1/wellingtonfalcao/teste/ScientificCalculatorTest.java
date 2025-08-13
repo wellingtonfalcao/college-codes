7ad626f618b98cd53b0b45670b48bd09310c6fd2
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-08-13 15:37:18 -0300

    Update: TP1 of Java finished

--- java/tp1-desenv-java-wellington-falcao/src/test/java/tp1/wellingtonfalcao/teste/ScientificCalculatorTest.java
+++ java/tp1-desenv-java-wellington-falcao/src/test/java/tp1/wellingtonfalcao/teste/ScientificCalculatorTest.java
@@ -1,7 +1,10 @@
 package tp1.wellingtonfalcao.teste;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import tp1.wellingtonfalcao.teste.main.domain.ScientificCalculator;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +18,7 @@ public class ScientificCalculatorTest {
         calc = new ScientificCalculator();
     }
     @Test
+    @DisplayName("Deve realizar a soma de 2 números corretamente")
     void deveSomar_QuandoNumeros_EntaoRetornarSomaCorreta() {
 
         //Dado - Setup
@@ -29,6 +33,7 @@ public class ScientificCalculatorTest {
     }
 
     @Test
+    @DisplayName("Deve realizar a subtração de 2 números corretamente")
     void deveSubtrair_QuandoNumeros_EntaoRetornarSubtracaoCorreta() {
 
         //Dado - Setup
@@ -44,6 +49,7 @@ public class ScientificCalculatorTest {
     }
 
     @Test
+    @DisplayName("Deve tirar a raiz quadrada de um número positivo corretamente")
     void deveTirarRaizQuadrada_QuandoNumero_EntaoRetornarRaizQuadradaCorreta() {
 
         //Dado - Setup
@@ -58,7 +64,8 @@ public class ScientificCalculatorTest {
     }
 
     @Test
-    void deveLancarExcecao_QuandoNumeroNegativo_EntaoRetornarIllegalArgumentException() {
+    @DisplayName("Deve lançar uma exceção caso o número parar tirar raiz quadrada seja negativo")
+    void deveLancarExcecaodaRaizQuadrada_QuandoNumeroNegativo_EntaoRetornarIllegalArgumentException() {
 
         //Dado - Setup
         double numero = -70;
@@ -73,6 +80,7 @@ public class ScientificCalculatorTest {
     }
 
     @Test
+    @DisplayName("Deve lançar uma exceção caso o divisor for zero")
     void deveLancarExcecao_QuandoDivisorForZero_EntaoRetornarIllegalArgumentException() {
 
         //Dado - Setup
@@ -87,4 +95,42 @@ public class ScientificCalculatorTest {
         //Então - Assertion
         assertEquals(msgEsperada, excecaoLancada.getMessage(), "A mensagem da exception está incorreta!");
     }
+
+//    ## 8. Combinando múltiplas entradas (log e seno)
+//
+//    - Implemente testes para os métodos `log` e `sin`.
+//    - Garanta a precisão dos resultados esperados para valores positivos comuns.
+
+    @Test
+    @DisplayName("Deve calcular o log de um número positivo corretamente.")
+    void deveCalcularLog_QuandoNumeroPositivo_EntaoRetornarValorCorreto() {
+        //Dado - Setup
+        double numero = 8;
+        double resultadoEsperado = 2.079;
+
+        //Quando - Execution
+        double resultadoAtual = calc.log(numero);
+
+        //Então - Assertion
+        assertEquals(resultadoEsperado, resultadoAtual, 0.001, "O Logaritmo não está correto");
+    }
+
+    @ParameterizedTest(name = "O seno de {0} deve ser {1}.")
+    @CsvSource({
+            "0.0,0.0",
+            "30.0,0.5",
+            "90.0,1",
+            "180.0,0",
+    })
+    @DisplayName("Deve calcular o valor correto do seno para alguns ângulos.")
+    void deveCalcular_QuandoAngulos_EntaoRetornarValorCorreto(double grau, double seno) {
+        //Dado (No CsvSource)
+
+        //Quando
+        double novoSeno = calc.sin(grau);
+
+        //Então
+        assertEquals(seno, novoSeno, 0.01, "O seno de %.2f deveria ser %.2f".formatted(grau, seno, novoSeno));
+    }
+
 }
